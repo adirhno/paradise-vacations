2dcfc34aaead6eaffdc6a36e5b883f5ab6102f8c
adirhno <adirhno@gmail.com>
2022-09-10 03:30:08 +0300

    'a'

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post("/add-follower", (req, res) => {
 
 app.get("/profile", (req, res) => {
   if (req.session.logged || req.session.googLogged) {
-    if (req.session.googLogged) {
+    if (req.session.googLogged > 1) {
       res.render("profile", {
         user: req.session.googLogged,
         layout: "default",
